7dc0adedd048887d7bb28dbfd7cb51901763fd40
sakamot <matuhana.8@gmail.com>
2018-02-25 22:55:11 +0900

    refactor get_info.py

--- get_info.py
+++ get_info.py
@@ -5,24 +5,24 @@ from slackbot.bot import Bot
 import requests
 import json
 
-# アクセスするURL
-url = "https://transit.yahoo.co.jp/traininfo/area/4/"
-html = urllib.request.urlopen(url)
-soup = BeautifulSoup(html, "html.parser")
-delay_rosens = soup.find("div", attrs={"class": "elmTblLstLine trouble"})
-post_text = ""
+def get_delay_info():
+    url = "https://transit.yahoo.co.jp/traininfo/area/4/"
+    html = urllib.request.urlopen(url)
+    soup = BeautifulSoup(html, "html.parser")
+    div_trouble = soup.find("div", attrs={"class": "elmTblLstLine trouble"})
+    post_text = ""
 
-for rosen in delay_rosens.find_all('tr')[1:-1]:
-    a_tag = rosen.find('a')
-    link = "[" + a_tag.string + "](" + a_tag.attrs['href'] + ")"
-    status = rosen.find('span', attrs={"class": "colTrouble"}).string
-    text = rosen.find_all('td')[2].string
-    post_text += "*!" + status + "!* " + link + "\n" + text
+    for tr in div_trouble.find_all('tr')[1:-1]:
+        a_tag = tr.find('a')
+        link = "[" + a_tag.string + "](" + a_tag.attrs['href'] + ")"
+        status = tr.find('span', attrs={"class": "colTrouble"}).string
+        text = tr.find_all('td')[2].string
+        post_text += "*!" + status + "!* " + link + "\n" + text
 
-if not post_text:
-    post_text = '遅延してる路線はありません。'
+    notify_slack(post_text or '遅延してる路線はありません。')
 
-url = "your url"
-requests.post(url, data = json.dumps({
-    'text': post_text, # 投稿するテキスト
-}))
+def notify_slack(text):
+    url = "your url"
+    requests.post(url, data = json.dumps({
+        'text': text, # 投稿するテキスト
+    }))
