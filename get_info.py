4c624ec260d24829e8f397ad99b079a150c52899
sakamot <matuhana.8@gmail.com>
2018-03-03 15:22:21 +0900

    refactor: limit line to acquire

--- get_info.py
+++ get_info.py
@@ -12,12 +12,14 @@ def get_delay_info(event, context):
     soup = BeautifulSoup(html, "html.parser")
     div_trouble = soup.find("div", attrs={"class": "elmTblLstLine trouble"})
     fields = []
+    lines = ["小田急小田原線", "東京メトロ千代田線", "山手線", "中央線(快速)[東京～高尾]", "東京メトロ副都心線", "東急東横線", "東武東上線"]
 
     for tr in div_trouble.find_all('tr')[1:]:
         a_tag = tr.find('a')
-        status = tr.find('span', attrs={"class": "colTrouble"}).string
-        text = tr.find_all('td')[2].string
-        fields.append({"title": status + ": " + a_tag.string, "value": "<" + a_tag.attrs['href'] + "|" + text + ">"})
+        if a_tag.string in lines:
+            status = tr.find('span', attrs={"class": "colTrouble"}).string
+            text = tr.find_all('td')[2].string
+            fields.append({"title": status + ": " + a_tag.string, "value": "<" + a_tag.attrs['href'] + "|" + text + ">"})
 
     notify_slack(fields)
     return 'Success!'
